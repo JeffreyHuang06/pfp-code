31aa7a28ad520682503bc90aed3e94ad247f0077
FeverDreme <jeffreyhuang08@gmail.com>
2021-04-26 03:13:02 -0400

    added problems and testcase types

---
+++ api/problems/problemType.ts
@@ -0,0 +1,4 @@
+export type testCase = {
+    input: string;
+    output: string;
+};
---
+++ api/problems/problems.ts
@@ -0,0 +1,16 @@
+import { testCase } from "./problemType"
+
+let problems: Map<string, testCase[]> = new Map();
+
+problems.set("Test", [
+    {
+        input: "1",
+        output: "1"
+    }
+    ,{
+        input: "1",
+        output: "2"
+    }
+])
+
+export default problems
