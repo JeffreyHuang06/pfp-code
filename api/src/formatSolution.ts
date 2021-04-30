6c7c60c09b50aa76e021c6af5892c628dcbfa1e3
FeverDreme <jeffreyhuang08@gmail.com>
2021-04-30 01:48:56 -0400

    added codeProtoType typgin

--- api/src/formatSolution.ts
+++ api/src/formatSolution.ts
@@ -1,4 +1,4 @@
-import {codeProto, langVers} from "./codeProto.js"
+import {codeProto, codeProtoType, langVers} from "./codeProto.js"
 
 export const validateCodeInput = (bodyJSON: any) => {
     return (bodyJSON.hasOwnProperty("lang") && bodyJSON.hasOwnProperty("code") && bodyJSON.hasOwnProperty("prob"));
@@ -7,10 +7,10 @@ export const validateCodeInput = (bodyJSON: any) => {
 export const formatSolution = (bodyJSON: any) => {
     const {lang, code} = bodyJSON;
 
-    const codeUpload = codeProto;
+    const codeUpload: codeProtoType = codeProto;
 
     codeUpload.language = lang;
-    codeUpload.version = langVers.get(lang);
+    codeUpload.version = langVers.get(lang) || "";
     codeUpload.files[0].content = code;
 
     return codeUpload;
