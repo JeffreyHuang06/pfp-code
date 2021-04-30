9720b0890e3f078bda9889b6c4e8a709424696a7
FeverDreme <jeffreyhuang08@gmail.com>
2021-04-30 01:17:40 -0400

    renoved td wrappers

--- app/src/components/SolutionStatus.tsx
+++ app/src/components/SolutionStatus.tsx
@@ -9,30 +9,22 @@ const getSolBlock = (returnString: string): JSX.Element => {
     switch(returnString){
         case "SUCCESS":
             return (
-                <td>
-                    <img className="mr-3" src={SuccessSVG} alt="success" style={{width: "5em"}}/>
-                </td>
+                <img className="mr-3" src={SuccessSVG} alt="success" style={{width: "5em"}}/>
             );
         
         case "FAILED":
             return (
-                <td>
-                    <img className="mr-3" src={FailSVG} alt="fail" style={{width: "5em"}}/>
-                </td>
+                <img className="mr-3" src={FailSVG} alt="fail" style={{width: "5em"}}/>
             );
 
         case "PISTONERR":
             return (
-                <td>
-                    <img className="mr-3" src={ErrSVG} alt="fail" style={{width: "5em"}}/>
-                </td>
+                <img className="mr-3" src={ErrSVG} alt="fail" style={{width: "5em"}}/>
             );
         
         case "SOLERR":
             return (
-                <td>
-                    <img className="mr-3" src={ErrSVG} alt="fail" style={{width: "5em"}}/>
-                </td>
+                <img className="mr-3" src={ErrSVG} alt="fail" style={{width: "5em"}}/>
             );
 
         default:
@@ -44,11 +36,11 @@ const SolutionStatus = () => {
     const status = useRecoilValue(statusAtom);
 
     return (
-        <ul className="border border-secondary rounded container mt-3 py-3">
+        <div className="border border-secondary rounded container mt-3 py-3">
             {
                 status.map(returnString => getSolBlock(returnString))
             }
-        </ul>
+        </div>
     )
 }
 
