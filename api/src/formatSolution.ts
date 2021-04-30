8a81628101cc5bb5f8cf8e14e4fbc1d86ea39c29
FeverDreme <jeffreyhuang08@gmail.com>
2021-04-30 01:50:09 -0400

    arg typing

--- api/src/formatSolution.ts
+++ api/src/formatSolution.ts
@@ -1,10 +1,12 @@
+import { clientSubmission } from "types/clientSubmissionType.js";
 import {codeProto, codeProtoType, langVers} from "./codeProto.js"
 
+// bodyJSON but be any type because possibly malformed user request
 export const validateCodeInput = (bodyJSON: any) => {
     return (bodyJSON.hasOwnProperty("lang") && bodyJSON.hasOwnProperty("code") && bodyJSON.hasOwnProperty("prob"));
 }
 
-export const formatSolution = (bodyJSON: any) => {
+export const formatSolution = (bodyJSON: clientSubmission) => {
     const {lang, code} = bodyJSON;
 
     const codeUpload: codeProtoType = codeProto;
